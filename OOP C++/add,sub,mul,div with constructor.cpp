80ad29d053289514697b4d3286ecf71ee778abc1
Sherwin <110648990+sherwin1004@users.noreply.github.com>
2022-11-30 17:56:59 +0530

    Add files via upload

---
+++ OOP C++/add,sub,mul,div with constructor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  double firstNumber, secondNumber;
+
+  cout << "Enter the first number: " << endl;
+  cin >> firstNumber;
+
+  cout << "Enter the second number: " << endl;
+  cin >> secondNumber;
+
+  cout << "firstNumber + secondNumber = " << firstNumber + secondNumber << endl;
+
+  cout << "firstNumber - secondNumber = " << firstNumber - secondNumber << endl;
+
+  cout << "firstNumber * secondNumber = " << firstNumber * secondNumber << endl;
+
+  cout << "firstNumber/secondNumber = " << firstNumber / secondNumber << endl;
+  return 0;
+}
